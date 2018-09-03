739e91569573f068c1aa15cc86fa263c757c705f
Brendan O <brendan.orenstein@anu.edu.au>
2018-09-03 17:22:44 +1000

    Fixed reduction_script
    Made the reduction_script run successfully on my local machine, implemented read_and_overscan_correct

--- reduction_script.py
+++ reduction_script.py
@@ -23,23 +23,53 @@ from veloce_reduction.extraction import extract_spectrum, extract_spectrum_from_
 from veloce_reduction.relative_intensities import get_relints, get_relints_from_indices, append_relints_to_FITS
 from veloce_reduction.get_info_from_headers import get_obs_coords_from_header
 
-bias_list = glob.glob("/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/bias*")
+from veloce_reduction.subtract_overscan import read_and_overscan_correct
 
-print("bias_list", bias_list)
+import warnings
+warnings.filterwarnings('ignore')
 
-#stellar_list = 
+
+bias_list = ["/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/bias1.fits", "/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/bias2.fits", "/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/bias5.fits"]
+
+s_list = glob.glob("/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/ccd_3/15aug*")
+c_list = glob.glob("/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180815/ccd_3/*overscan_corrected*")
+stellar_list = list(set(s_list)-set(c_list))
+
+white_list = ['/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_0.5.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_4.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_5.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_6.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_noap.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_noap_2.fits', '/Users/Brendan/Dropbox/Brendan/Veloce/Data/veloce/180813/flat_noap_3.fits']
+
+path = '/Users/Brendan/Dropbox/Brendan/Veloce/Data/output3'
+
+saveall = '/Users/Brendan/Dropbox/Brendan/Veloce/Data'
+
+corrected_bias_list = []
+for i in bias_list:
+    corrected_bias_list.append(read_and_overscan_correct(i))
+
+corrected_white_list = []
+for i in white_list:
+    corrected_white_list.append(read_and_overscan_correct(i))
+
+corrected_stellar_list = []
+for i in stellar_list:
+    corrected_stellar_list.append(read_and_overscan_correct(i))
+
+print(corrected_stellar_list)
+
+#print("pyfits.getdata(bias_list[0].shape", pyfits.getdata(bias_list[0]).shape)
+#print("pyfits.getdata(corrected_bias_list[0].shape", pyfits.getdata(corrected_bias_list[0]).shape)
 
 gain = [0.88, 0.93, 0.99, 0.93] #(depending on the camera setup)
 nx=4112
 ny=4096
-medbias,coeffs,offsets,rons = get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5., gain=gain, debug_level=0, timit=True)
+medbias,coeffs,offsets,rons = get_bias_and_readnoise_from_bias_frames(corrected_bias_list, degpol=5, clip=5., gain=gain, debug_level=0, timit=True)
 # create MASTER BIAS frame and read-out noise mask (units = electrons)
 offmask,ronmask = make_offmask_and_ronmask(offsets, rons, nx, ny, gain=gain, savefiles=True, path=path, timit=True)
 MB = make_master_bias_from_coeffs(coeffs, nx, ny, savefile=True, path=path, timit=True)
 #we did not have darks, so I did this
 MD = np.zeros(MB.shape)
+
 #create (bias- & dark-subtracted) MASTER WHITE frame and corresponding error array (units = ADUs)
-MW,err_MW = process_whites(white_list, MB=MB, ronmask=ronmask, MD=MD, gain=gain, scalable=False, fancy=False, clip=5., savefile=True, saveall=True, diffimg=False, path=None, timit=False)
+MW,err_MW = process_whites(white_list, corrected_white_list, MB=MB, ronmask=ronmask, MD=MD, gain=gain, scalable=False, fancy=False, clip=5., savefile=True, saveall=True, diffimg=False, path=path, timit=False)
 # find orders
 P,tempmask = find_stripes(MW, deg_polynomial=2, min_peak=0.05, gauss_filter_sigma=3., simu=False)
 # assign physical diffraction order numbers (this is only a dummy function for now) to order-fit polynomials and bad-region masks
@@ -47,14 +77,14 @@ P_id = make_P_id(P)
 mask = make_mask_dict(tempmask)
  
 #loop over all files you want to extract a 1-dim spectrum for
-for filename in stellar_list:
+for filename in corrected_stellar_list:
     #do some housekeeping with filenames
     dum = filename.split('/')
     dum2 = dum[-1].split('.')
     obsname = dum2[0]
  
     # (1) call routine that does all the bias and dark correction stuff and proper error treatment
-    img = correct_for_bias_and_dark_from_filename(filename, MB, MD, gain=gain, scalable=False, savefile=saveall, path=path, timit=True)   #[e-]
+    img = correct_for_bias_and_dark_from_filename(filename, MB, MD, gain=gain, scalable=False, savefile=True, path=path, timit=True)   #[e-]
     err_img = np.sqrt(np.clip(img,0,None) + ronmask*ronmask)   # [e-]
  
     # (5) extract stripes
