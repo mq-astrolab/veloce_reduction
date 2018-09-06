b55675d86f46f36ecff128f39ae8211f0a2dee14
Mike Ireland <michael.ireland@anu.edu.au>
2018-09-06 10:12:36 +1000

    Added simple dark script

---
+++ utils/dark.py
@@ -0,0 +1,42 @@
+from __future__ import division, print_function
+import astropy.io.fits as pyfits
+import numpy as np
+import matplotlib.pyplot as plt
+import subtract_overscan as so
+import glob
+import scipy.ndimage as nd
+
+files = glob.glob('/Users/mireland/data/veloce/180815/ccd_3/15aug3022?.fits')
+darks_sso = []
+for f in files:
+    print(f)
+    darks_sso.append(so.read_and_overscan_correct(f))
+darks_sso = np.array(darks_sso)
+    
+    
+files = glob.glob('/Users/mireland/data/veloce/Video_Offsets/*fit')
+biases = []
+for f in files:
+    print(f)
+    biases.append(so.read_and_overscan_correct(f))
+biases = np.array(biases)
+
+files = glob.glob('/Users/mireland/data/veloce/Dark_25_8_2018/Bias_Pre_Dark*.fit')
+#files = glob.glob('/Users/mireland/data/veloce/Dark_27_7_2018/Bias_Cold_10?.fit')
+dark_biases = []
+for f in files:
+    print(f)
+    dark_biases.append(so.read_and_overscan_correct(f))
+dark_biases = np.array(dark_biases)
+
+files = glob.glob('/Users/mireland/data/veloce/Dark_25_8_2018/Dark_2_Hour*.fit')
+#files = glob.glob('/Users/mireland/data/veloce/Dark_27_7_2018/Exp_1h_Cold_10[45].fit')
+darks = []
+for f in files:
+    print(f)
+    darks.append(so.read_and_overscan_correct(f))
+darks = np.array(darks)
+
+mn_bias = np.mean(dark_biases, axis=0)
+for d in darks:
+    print(np.median(d - mn_bias))
