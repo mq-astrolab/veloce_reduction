ongoing development; fixed major bug in dark current calculation (when there are darks of different exposure times) and subtraction (scaled MD was multiplied with texp in two different places)

process_scripts.py
@@ -10,14 +10,14 @@ import time
 import os
 import barycorrpy
 
-from veloce_reduction.helper_functions import binary_indices
-from veloce_reduction.calibration import correct_for_bias_and_dark_from_filename
-from veloce_reduction.cosmic_ray_removal import remove_cosmics
-from veloce_reduction.background import remove_background
-from veloce_reduction.order_tracing import extract_stripes
-from veloce_reduction.extraction import extract_spectrum, extract_spectrum_from_indices
-from veloce_reduction.relative_intensities import get_relints, get_relints_from_indices, append_relints_to_FITS
-from veloce_reduction.get_info_from_headers import get_obs_coords_from_header
+from veloce_reduction.veloce_reduction.helper_functions import binary_indices
+from veloce_reduction.veloce_reduction.calibration import correct_for_bias_and_dark_from_filename
+from veloce_reduction.veloce_reduction.cosmic_ray_removal import remove_cosmics
+from veloce_reduction.veloce_reduction.background import remove_background
+from veloce_reduction.veloce_reduction.order_tracing import extract_stripes
+from veloce_reduction.veloce_reduction.extraction import extract_spectrum, extract_spectrum_from_indices
+from veloce_reduction.veloce_reduction.relative_intensities import get_relints, get_relints_from_indices, append_relints_to_FITS
+from veloce_reduction.veloce_reduction.get_info_from_headers import get_obs_coords_from_header
 
 
 
@@ -53,11 +53,14 @@ def process_whites(white_list, MB=None, ronmask=None, MD=None, gain=None, scalab
     if timit:
         start_time = time.time()
 
+    if debug_level >= 1:
+        print('Creating master white frame from '+str(len(white_list))+' fibre flats...')       
+
     #if the darks have a different exposure time than the whites, then we need to re-scale the master dark
     try:
         texp = pyfits.getval(white_list[0], 'exptime')
     except:
-        texp = pyfits.getval(white_list[0], 'EXPOSED')
+        texp = pyfits.getval(white_list[0], 'TOTALEXP')
 
     #if INPUT arrays are not given, read them from default files
     if path is None:
@@ -92,7 +95,7 @@ def process_whites(white_list, MB=None, ronmask=None, MD=None, gain=None, scalab
             print('Now processing file: '+str(fn))
         #call routine that does all the bias and dark correction stuff and converts from ADU to e-
         img = correct_for_bias_and_dark_from_filename(fn, MB, MD, gain=gain, scalable=scalable, savefile=saveall, path=path, timit=timit)     #these are now bias- & dark-corrected images; units are e-
-        if debug_level >=1:
+        if debug_level >=2:
             print('min(img) = '+str(np.min(img)))
         allimg.append(img)
 #         allerr.append(err)
@@ -208,9 +211,6 @@ def process_science_images(imglist, P_id, mask=None, sampling_size=25, slit_heig
     ### (1) bias and dark subtraction ###
     #####################################
     
-    #if the darks have a different exposure time than the science images, then we need to re-scale the master dark
-    texp = pyfits.getval(imglist[0], 'exptime')
-    
     #if INPUT arrays are not given, read them from default files
     if path is None:
         print('WARNING: output file directory not provided!!!')
@@ -241,26 +241,29 @@ def process_science_images(imglist, P_id, mask=None, sampling_size=25, slit_heig
         dum = filename.split('/')
         dum2 = dum[-1].split('.')
         obsname = dum2[0]
-        
+              
         # (1) call routine that does all the bias and dark correction stuff and proper error treatment
-        img = correct_for_bias_and_dark_from_filename(filename, MB, MD, gain=gain, scalable=False, savefile=saveall, path=path, timit=True)   #[e-]
+        img = correct_for_bias_and_dark_from_filename(filename, MB, MD, gain=gain, scalable=scalable, savefile=saveall, path=path, timit=True)   #[e-]
         #err = np.sqrt(img + ronmask*ronmask)   # [e-]
         #TEMPFIX:
         err_img = np.sqrt(np.clip(img,0,None) + ronmask*ronmask)   # [e-]
         
         # (2) remove cosmic rays (ERRORS REMAIN UNCHANGED)
-        cosmic_cleaned_img = remove_cosmics(img, ronmask, obsname, path, Flim=3.0, siglim=5.0, maxiter=1, savemask=True, savefile=True, save_err=False, verbose=True, timit=True)   # [e-]
+        # cosmic_cleaned_img = remove_cosmics(img, ronmask, obsname, path, Flim=3.0, siglim=5.0, maxiter=1, savemask=True, savefile=True, save_err=False, verbose=True, timit=True)   # [e-]
         #adjust errors?
         
         # (3) fit and remove background (ERRORS REMAIN UNCHANGED)
-        bg_corrected_img = remove_background(cosmic_cleaned_img, P_id, obsname, path, degpol=5, slit_height=slit_height, save_bg=True, savefile=True, save_err=False,
-                                             exclude_top_and_bottom=True, verbose=True, timit=True)   # [e-]
+        # bg_corrected_img = remove_background(cosmic_cleaned_img, P_id, obsname, path, degpol=5, slit_height=slit_height, save_bg=True, savefile=True, save_err=False,
+        #                                      exclude_top_and_bottom=True, verbose=True, timit=True)   # [e-]
+        # bg_corrected_img = remove_background(img, P_id, obsname, path, degpol=5, slit_height=slit_height, save_bg=False, savefile=True, save_err=False,
+        #                                      exclude_top_and_bottom=True, verbose=True, timit=True)   # [e-]
         #adjust errors?
 
         # (4) remove pixel-to-pixel sensitivity variations (2-dim)
         #XXXXXXXXXXXXXXXXXXXXXXXXXXX
         #TEMPFIX
-        final_img = bg_corrected_img.copy()   # [e-]
+        # final_img = bg_corrected_img.copy()   # [e-]
+        final_img = img.copy()   # [e-]
         #adjust errors?
 
         # (5) extract stripes
@@ -270,38 +273,40 @@ def process_science_images(imglist, P_id, mask=None, sampling_size=25, slit_heig
 
         # (6) perform extraction of 1-dim spectrum
         if from_indices:
+            pix,flux,err = extract_spectrum_from_indices(final_img, err_img, stripe_indices, method='quick', slit_height=slit_height, RON=ronmask, savefile=True, 
+                                                         filetype='fits', obsname=obsname, path=path, timit=True)
             pix,flux,err = extract_spectrum_from_indices(final_img, err_img, stripe_indices, method=ext_method, slit_height=slit_height, RON=ronmask, savefile=True, 
                                                          filetype='fits', obsname=obsname, path=path, timit=True)
         else:
             pix2,flux2,err2 = extract_spectrum(stripes, err_stripes=err_stripes, ron_stripes=ron_stripes, method=ext_method, slit_height=slit_height, RON=ronmask, savefile=False, 
                                             filetype='fits', obsname=obsname, path=path, timit=True)
     
-        # (7) get relative intensities of different fibres
-        if from_indices:
-            relints = get_relints_from_indices(P_id, final_img, err_img, stripe_indices, mask=mask, sampling_size=sampling_size, slit_height=slit_height, return_full=False, timit=True) 
-        else:
-            relints = get_relints(P_id, stripes, err_stripes, mask=mask, sampling_size=sampling_size, slit_height=slit_height, return_full=False, timit=True)
-
-    
-        # (8) get wavelength solution
-        #XXXXX
-
-
-        # (9) get barycentric correction
-        lat, long, alt = get_obs_coords_from_header(fn)
-        bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, lat=lat, longi=long, alt=float(alt), ephemeris='de430', zmeas=0.0)
-        #bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, lat=-31.2755, longi=149.0673, alt=1165.0, ephemeris='de430', zmeas=0.0)
-        #bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, obsname='AAO', ephemeris='de430')
-
-        #now append relints, wl-solution, and barycorr to extracted FITS file header
-        outfn = path + obsname + '_extracted.fits'
-        if os.path.isfile(outfn):
-            #relative fibre intensities
-            dum = append_relints_to_FITS(relints, outfn, nfib=19)
-            #wavelength solution
-            #pyfits.setval(fn, 'RELINT' + str(i + 1).zfill(2), value=relints[i], comment='fibre #' + str(fibnums[i]) + ' - ' + fibinfo[i] + ' fibre')
-            #barycentric correction
-            pyfits.setval(outfn, 'BARYCORR', value=np.array(bc[0])[0], comment='barycentric correction [m/s]')
+#         # (7) get relative intensities of different fibres
+#         if from_indices:
+#             relints = get_relints_from_indices(P_id, final_img, err_img, stripe_indices, mask=mask, sampling_size=sampling_size, slit_height=slit_height, return_full=False, timit=True) 
+#         else:
+#             relints = get_relints(P_id, stripes, err_stripes, mask=mask, sampling_size=sampling_size, slit_height=slit_height, return_full=False, timit=True)
+# 
+#     
+#         # (8) get wavelength solution
+#         #XXXXX
+# 
+# 
+#         # (9) get barycentric correction
+#         lat, long, alt = get_obs_coords_from_header(fn)
+#         bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, lat=lat, longi=long, alt=float(alt), ephemeris='de430', zmeas=0.0)
+#         #bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, lat=-31.2755, longi=149.0673, alt=1165.0, ephemeris='de430', zmeas=0.0)
+#         #bc = barycorrpy.get_BC_vel(JDUTC=JDUTC, hip_id=8102, obsname='AAO', ephemeris='de430')
+# 
+#         #now append relints, wl-solution, and barycorr to extracted FITS file header
+#         outfn = path + obsname + '_extracted.fits'
+#         if os.path.isfile(outfn):
+#             #relative fibre intensities
+#             dum = append_relints_to_FITS(relints, outfn, nfib=19)
+#             #wavelength solution
+#             #pyfits.setval(fn, 'RELINT' + str(i + 1).zfill(2), value=relints[i], comment='fibre #' + str(fibnums[i]) + ' - ' + fibinfo[i] + ' fibre')
+#             #barycentric correction
+#             pyfits.setval(outfn, 'BARYCORR', value=np.array(bc[0])[0], comment='barycentric correction [m/s]')
 
 
     if timit:

veloce_reduction/calibration.py
@@ -17,13 +17,14 @@ from veloce_reduction.veloce_reduction.helper_functions import correct_orientati
 
 
 
-def make_median_image(imglist, MB=None, raw=False):
+def make_median_image(imglist, MB=None, scalable=False, raw=False):
     """
     Make a median image from a given list of images.
 
     INPUT:
     'imglist'  : list of files (incl. directories)
     'MB'       : master bias frame - if provided, it will be subtracted from every image before median image is computed
+    'scalable' : boolean - do you want to scale this to an exposure time of 1s (AFTER the bias is subtracted!!!!!)
     'raw'      : boolean - set to TRUE if you want to retain the original size and orientation;
                  otherwise the image will be brought to the 'correct' orientation and the overscan regions will be cropped
 
@@ -38,6 +39,7 @@ def make_median_image(imglist, MB=None, raw=False):
 
     # loop over all files in "dark_list"
     for file in imglist:
+        
         # read in dark image
         img = pyfits.getdata(file)
         if not raw:
@@ -48,6 +50,9 @@ def make_median_image(imglist, MB=None, raw=False):
         if MB is not None:
             # subtract master bias (if provided)
             img = img - MB
+        if scalable:
+            texp = pyfits.getval(file, 'TOTALEXP')
+            img /= texp
 
         # add image to list
         allimg.append(img)
@@ -380,7 +385,7 @@ def get_bias_and_readnoise_from_overscan(img, timit=False):
 
 
 
-def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=None, debug_level=0, timit=False):
+def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=None, save_medimg=True, debug_level=0, timit=False):
     """
     Calculate the median bias frame, the offsets in the four different quadrants (assuming bias frames are flat within a quadrant),
     and the read-out noise per quadrant (ie the STDEV of the signal, but from difference images).
@@ -390,6 +395,7 @@ def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=No
     'degpol'       : order of the polynomial (in each direction) to be used in the 2-dim polynomial surface fits to each quadrant's median bais frame
     'clip'         : number of 'sigmas' used to identify outliers when 'cleaning' each quadrant's median bais frame before the surface fitting
     'gain'         : array of gains for each quadrant (in units of e-/ADU)
+    'save_medimg'  : boolean - do you want to save the median image to a FITS file?
     'debug_level'  : for debugging...
     'timit'        : boolean - do you want to measure execution run time?
     
@@ -437,25 +443,22 @@ def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=No
     sigs_q4 = []
     allimg = []
 
-    # for name in bias_list:
-    #
-    #     img = pyfits.getdata(name)
-    #     print(img.shape)
+    if debug_level >= 1:
+        print('Determining bias levels and read-out noise from '+str(len(bias_list))+' bias frames...')
 
     #first get mean / median for all bias images (per quadrant)
     for name in bias_list:
-
+        
+        if debug_level >= 1:
+            print('OK, reading ',name)
+        
         img = pyfits.getdata(name)
+        
         #bring to "correct" orientation
         img = correct_orientation(img)
-
-        # ny,nx = img.shape
-        #
-        # #define four quadrants via masks
-        # q1,q2,q3,q4 = make_quadrant_masks(nx,ny)
-
         #remove the overscan region, which looks crap for actual bias images
         img = crop_overscan_region(img)
+        
         #means_q1.append(np.nanmean(img[q1]))
         medians_q1.append(np.nanmedian(img[q1]))
         #means_q2.append(np.nanmean(img[q2]))
@@ -470,6 +473,7 @@ def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=No
     # by using the difference images we are less susceptible to funny pixels (hot, warm, cosmics, etc.)
     list_of_combinations = list(combinations(bias_list, 2))
     for (name1,name2) in list_of_combinations:
+        
         # read in observations and bring to right format
         img1 = pyfits.getdata(name1)
         img2 = pyfits.getdata(name2)
@@ -535,7 +539,7 @@ def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=No
     #return all coefficients as 4-element array
     coeffs = np.array([coeffs_q1, coeffs_q2, coeffs_q3, coeffs_q4])            
 
-    #convert read-out noise (but NOT offsets!!!) to units of electrons rather than ADUs by muliplying with the gain (which has units of e-/ADU)
+    #convert read-out noise (but NOT offsets!!!) to units of electrons rather than ADUs by multiplying with the gain (which has units of e-/ADU)
     if gain is None:
         print('ERROR: gain(s) not set!!!')
         return
@@ -550,19 +554,14 @@ def get_bias_and_readnoise_from_bias_frames(bias_list, degpol=5, clip=5, gain=No
     if timit:
         print('Time elapsed: '+str(np.round(time.time() - start_time,1))+' seconds')
 
-    if savefile:
+    if save_medimg:
         #save median bias image
         dum = bias_list[0].split('/')
         path = bias_list[0][0:-len(dum[-1])]
-        #get header from the read-noise mask file
-        h = pyfits.getheader(path+'read_noise_mask.fits')
-        #change a few things
-        for i in range(1,5):
-            del h['offset_'+str(i)]
-        h['UNITS'] = 'ADU'
-        h['HISTORY'][0] = ('   median BIAS frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)')
-        #write master bias to file
-        pyfits.writeto(path+'median_bias.fits', medimg, h, clobber=True)
+        #write median bias image to file
+        pyfits.writeto(path+'median_bias.fits', medimg, clobber=True)
+        pyfits.setval(path+'median_bias.fits', 'UNITS', value='ADU')
+        pyfits.setval(path+'median_bias.fits', 'HISTORY', value='   master BIAS frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)')
 
     return medimg,coeffs,offsets,rons
 
@@ -620,10 +619,10 @@ def make_offmask_and_ronmask(offsets, rons, nx, ny, gain=None, savefiles=False, 
             print('ERROR: output file directory not provided!!!')
             return
         else:
-            #write master bias to file
+            #write offmask to file
             pyfits.writeto(path+'offmask.fits', offmask, clobber=True)
             pyfits.setval(path+'offmask.fits', 'UNITS', value='ADU')
-            pyfits.setval(path+'offmask.fits', 'HISTORY', value='   master BIAS frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)')
+            pyfits.setval(path+'offmask.fits', 'HISTORY', value='   offset mask - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)')
             if nq == 1:
                 pyfits.setval(path+'offmask.fits', 'OFFSET', value=offsets, comment='in ADU')
                 pyfits.setval(path+'offmask.fits', 'RNOISE', value=rons, comment='in ELECTRONS')
@@ -741,20 +740,21 @@ def make_master_bias_from_coeffs(coeffs, nx, ny, savefile=False, path=None, timi
 
 
 
-def make_master_dark(dark_list, MB, gain=None, scalable=False, savefile=True, path=None, timit=False):
+def make_master_dark(dark_list, MB, gain=None, scalable=False, savefile=True, path=None, debug_level=0, timit=False):
     """
     This routine creates a "MASTER DARK" frame from a given list of dark frames. It also subtracts the MASTER BIAS from each dark frame before 
     combining them into the master dark frame.
     NOTE: the output is in units of ELECTRONS!!!
     
     INPUT:
-    'dark_list'  : list of raw dark image files (incl. directories)
-    'MB'         : the master bias frame [ADU]
-    'gain'       : the gains for each quadrant [e-/ADU]
-    'scalable'   : boolean - do you want to normalize the dark current to an exposure time of 1s? (ie do you want to make it "scalable"?)
-    'savefile'   : boolean - do you want to save the master dark frame to a fits file?
-    'path'       : path to the output file directory (only needed if savefile is set to TRUE)
-    'timit'      : boolean - do you want to measure execution run time?
+    'dark_list'    : list of raw dark image files (incl. directories)
+    'MB'           : the master bias frame [ADU]
+    'gain'         : the gains for each quadrant [e-/ADU]
+    'scalable'     : boolean - do you want to normalize the dark current to an exposure time of 1s? (ie do you want to make it "scalable"?)
+    'savefile'     : boolean - do you want to save the master dark frame to a fits file?
+    'path'         : path to the output file directory (only needed if savefile is set to TRUE)
+    'debug_level'  : for debugging...
+    'timit'        : boolean - do you want to measure execution run time?
     
     OUTPUT:
     'MD'  : the master dark frame [e-]
@@ -795,28 +795,56 @@ def make_master_dark(dark_list, MB, gain=None, scalable=False, savefile=True, pa
 # #     err_summed = np.sqrt(np.sum((np.array(allerr)**2),axis=0))
 # #     err_MD = err_summed / len(dark_list)
 
-    #get median image (including subtraction of master bias)
-    MD = make_median_image(dark_list, MB=MB, raw=False)
+    if debug_level >= 1:
+        print('Creating master dark frame from '+str(len(dark_list))+' dark frames...')       
 
-    #now convert to units of electrons
-    if gain is None:
-        print('ERROR: gain(s) not given!!!')
-        return
-    else:
-        ny,nx = MD.shape
-        q1,q2,q3,q4 = make_quadrant_masks(nx,ny)
-        MD[q1] = gain[0] * MD[q1]
-        MD[q2] = gain[1] * MD[q2]
-        MD[q3] = gain[2] * MD[q3]
-        MD[q4] = gain[3] * MD[q4]
-    
+    # get a list of all the exposure times first
+    exp_times = np.array([])
+    for file in sorted(dark_list):
+        exp_times =  np.append(exp_times, np.round(pyfits.getval(file,'TOTALEXP'),0))
 
-    #re-normalize to texp=1s to make it "scalable"
-    texp = pyfits.getval(dark_list[0], 'exptime')
+    # list of unique exposure times
+    unique_exp_times = np.array(list(sorted(set(exp_times))))
+    if debug_level >= 1 and len(unique_exp_times) > 1:
+        print('WARNING: not all dark frames have the same exposure times! Found '+str(len(unique_exp_times))+' unique exposure times!!!')
+
+
+    # create median dark files
     if scalable:
-        MD = MD / texp
-#         #but can't have that smaller than the RON!?!?!? this is taken care of in "correct_for_bias_and_dark(_from_filename)"
-#         err_MD = err_MD / texp
+        #get median image (including subtraction of master bias) and scale to texp=1s
+        MD = make_median_image(dark_list, MB=MB, scalable=scalable, raw=False)
+        if gain is None:
+            print('ERROR: gain(s) not given!!!')
+            return
+        else:
+            ny,nx = MD.shape
+            q1,q2,q3,q4 = make_quadrant_masks(nx,ny)
+            MD[q1] = gain[0] * MD[q1]
+            MD[q2] = gain[1] * MD[q2]
+            MD[q3] = gain[2] * MD[q3]
+            MD[q4] = gain[3] * MD[q4]
+    else:
+        # make dark "sublists" for all unique exposure times
+        all_dark_lists = []
+        for i in range(len(unique_exp_times)):
+            all_dark_lists.append( np.array(dark_list)[np.argwhere(exp_times == unique_exp_times[i]).flatten()] )
+        #get median image (including subtraction of master bias) for each "sub-list"
+        MD = []
+        for sublist in all_dark_lists:
+            sub_MD = make_median_image(sublist, MB=MB, scalable=scalable, raw=False)
+            #now convert to units of electrons
+            if gain is None:
+                print('ERROR: gain(s) not given!!!')
+                return
+            else:
+                ny,nx = sub_MD.shape
+                q1,q2,q3,q4 = make_quadrant_masks(nx,ny)
+                sub_MD[q1] = gain[0] * sub_MD[q1]
+                sub_MD[q2] = gain[1] * sub_MD[q2]
+                sub_MD[q3] = gain[2] * sub_MD[q3]
+                sub_MD[q4] = gain[3] * sub_MD[q4]
+            MD.append(sub_MD)
+
 
     if savefile:
         if path is None:
@@ -826,17 +854,23 @@ def make_master_dark(dark_list, MB, gain=None, scalable=False, savefile=True, pa
             path = dark_list[0][0:-len(dum[-1])]
         if scalable:
             outfn = path+'master_dark_scalable.fits'
-        else:
-            outfn = path+'master_dark_t'+str(int(np.round(texp,0)))+'.fits'
-        #get header from master BIAS frame
-        h = pyfits.getheader(path+'master_bias.fits')
-        h['HISTORY'][0] = '   MASTER DARK frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
-        h['EXPTIME'] = (texp, 'exposure time [s]')
-        h['UNITS'] = 'ELECTRONS'
-        if scalable:
+            #get header from master BIAS frame
+            h = pyfits.getheader(path+'master_bias.fits')
+            h['HISTORY'][0] = '   MASTER DARK frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
+            h['EXPTIME'] = (texp, 'exposure time [s]')
+            h['UNITS'] = 'ELECTRONS'
             h['COMMENT'] = 're-normalized to texp=1s to make it scalable'
-            h['EXPTIME'] = (1., 'exposure time [s]; originally '+str(np.round(texp,2))+'s')
-        pyfits.writeto(outfn, MD, h, clobber=True)
+            h['TOTALEXP'] = (1., 'exposure time [s]')
+            pyfits.writeto(outfn, MD, h, clobber=True)
+        else:
+            for i,submd in enumerate(MD):
+                outfn = path+'master_dark_t'+str(int(unique_exp_times[i]))+'.fits'
+                #get header from master BIAS frame
+                h = pyfits.getheader(path+'master_bias.fits')
+                h['HISTORY'][0] = '   MASTER DARK frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
+                h['TOTALEXP'] = (unique_exp_times[i], 'exposure time [s]')
+                h['UNITS'] = 'ELECTRONS'
+                pyfits.writeto(outfn, submd, h, clobber=True)
 #         if return_errors:
 #             h_err = h.copy()
 #             h_err['HISTORY'] = 'estimated uncertainty in MASTER DARK frame - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
@@ -914,6 +948,10 @@ def correct_for_bias_and_dark_from_filename(imgname, MB, MD, gain=None, scalable
     'MD'        : the master dark frame [e-]
     'gain'      : the gains for each quadrant [e-/ADU]
     'scalable'  : boolean - do you want to normalize the dark current to an exposure time of 1s? (ie do you want to make it "scalable"?)
+    'savefile'  : boolean - do you want to save the bias- & dark-corrected image (and corresponding error array) to a FITS file?
+    'path'      : output file directory
+    'simu'      : boolean - are you using Echelle++ simulated observations?
+    'timit'     : boolean - do you want to measure the execution run time?
     
     OUTPUT:
     'dc_bc_img'  : the bias- & dark-corrected image [e-] (also has been brought to 'correct' orientation and overscan regions cropped) 
@@ -939,9 +977,10 @@ def correct_for_bias_and_dark_from_filename(imgname, MB, MD, gain=None, scalable
 
 
     #(2) conversion to ELECTRONS and DARK SUBTRACTION [e-]
-    #if the darks have a different exposure time then the images you are trying to correct, we need to re-scale the master dark
+    #if the darks have a different exposure time than the images you are trying to correct, we need to re-scale the master dark
     if scalable:
-        texp = pyfits.getval(imgname, 'exptime')
+        # texp = pyfits.getval(imgname, 'exptime')
+        texp = pyfits.getval(imgname, 'TOTALEXP')
         if texp is not None:
             MD = MD * texp
 #             #cannot have an error estimate lower than the read-out noise; this is dodgy but don't know what else to do

veloce_reduction/extraction.py
@@ -10,7 +10,7 @@ import datetime
 import astropy.io.fits as pyfits
 import os
 
-from veloce_reduction.veloce_reduction.helper_functions import fibmodel_with_amp, make_norm_profiles_2, short_filenames
+from veloce_reduction.veloce_reduction.helper_functions import fibmodel_with_amp, make_norm_profiles_2, make_norm_profiles_3, short_filenames
 from veloce_reduction.veloce_reduction.spatial_profiles import fit_single_fibre_profile
 from veloce_reduction.veloce_reduction.linalg import linalg_extract_column
 from veloce_reduction.veloce_reduction.order_tracing import flatten_single_stripe, flatten_single_stripe_from_indices, extract_stripes
@@ -96,7 +96,7 @@ def quick_extract_from_indices(img, err_img, stripe_indices, slit_height=25, ver
     'stripe_indices' : dictionary (keys = orders) containing the indices of the pixels that are identified as the "stripes" (ie the to-be-extracted regions centred on the orders)
     'slit_height'    : height of the extraction slit (ie the pixel columns are 2*slit_height pixels long)
     'verbose'        : boolean - for debugging...
-    'timit'          : boolean - do youi want to measure execution run time?
+    'timit'          : boolean - do you want to measure execution run time?
     
     OUTPUT:
     'pixnum'  : dictionary (keys = orders) containing the pixel numbers (in dispersion direction)
@@ -529,7 +529,7 @@ def optimal_extraction(stripes, err_stripes=None, ron_stripes=None, nfib=28, RON
 
         # fix for order_01
         if ord == 'order_01':
-            for fib in sorted(pix[ord].keys()):
+            for fib in sorted(flux[ord].keys()):
                 flux['order_01'][fib] = np.r_[np.repeat(0., 900), flux['order_01'][fib]]
                 err['order_01'][fib] = np.r_[np.repeat(0., 900), err['order_01'][fib]]
 
@@ -772,7 +772,7 @@ def optimal_extraction_from_indices(img, stripe_indices, err_img=None, nfib=28, 
 
         # fix for order_01
         if ord == 'order_01':
-            for fib in sorted(pix[ord].keys()):
+            for fib in sorted(flux[ord].keys()):
                 flux['order_01'][fib] = np.r_[np.repeat(0., 900), flux['order_01'][fib]]
                 err['order_01'][fib] = np.r_[np.repeat(0., 900), err['order_01'][fib]]
 
@@ -868,6 +868,21 @@ def extract_spectrum(stripes, err_stripes, ron_stripes, method='optimal', indivi
     
     #now save to FITS file or PYTHON DICTIONARY if desired
     if savefile:
+        
+        # which extraction method was used exactly?
+        submethod = ''
+        if method.lower() == 'optimal':
+            if individual_fibres:
+                submethod = '3a'
+            else:
+                if combined_profiles:
+                    submethod = '3c'
+                else:
+                    submethod = '3b'
+            
+        # name of object
+        starname = pyfits.getval(path+obsname+'.fits', 'OBJECT')
+                     
         if path is None:
             print('ERROR: path to output directory not provided!!!')
             return
@@ -877,16 +892,14 @@ def extract_spectrum(stripes, err_stripes, ron_stripes, method='optimal', indivi
         else:
             while filetype not in ["fits", "dict", "both"]:
                 print('ERROR: file type for output file not recognized!')
-                filetype = raw_input('Which method do you want to use (valid options are ["fits" / "dict" / "both"] )?') 
+                filetype = raw_input('Which file type do you want to use (valid options are ["fits" / "dict" / "both"] )?') 
             if filetype in ['fits', 'both']:
-                #OK, save as FITS file
-                outfn = path+obsname+'_extracted.fits'
                 fluxarr = np.zeros((len(pix), len(pix['order_01'])))
                 errarr = np.zeros((len(pix), len(pix['order_01'])))
                 for i,o in enumerate(sorted(pix.keys())):
                     fluxarr[i,:] = flux[o]
                     errarr[i,:] = err[o]
-                #try and get header from previously saved files
+                # try and get header from previously saved files
                 if os.path.exists(path+obsname+'_BD_CR_BG_FF.fits'):
                     h = pyfits.getheader(path+obsname+'_BD_CR_BG_FF.fits')
                 elif os.path.exists(path+obsname+'_BD_CR_BG.fits'):
@@ -896,7 +909,7 @@ def extract_spectrum(stripes, err_stripes, ron_stripes, method='optimal', indivi
                 elif os.path.exists(path+obsname+'_BD.fits'):
                     h = pyfits.getheader(path+obsname+'_BD.fits')
                 else:
-                    h = pyfits.getheader(path+obsname+'.fits')
+                    h = pyfits.getheader(path+obsname+'.fits')   
                 #update the header and write to file
                 h['HISTORY'] = '   EXTRACTED SPECTRUM - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
                 h['METHOD'] = (method, 'extraction method used')
@@ -907,15 +920,9 @@ def extract_spectrum(stripes, err_stripes, ron_stripes, method='optimal', indivi
                 h['FIRSTORD'] = (topordnum, 'order number of first (top) order')
                 h['LASTORD'] = (botordnum, 'order number of last (bottom) order')
                 if method.lower() == 'optimal':
-                    if individual_fibres:
-                        submethod = '3a'
-                    else:
-                        if combined_profiles:
-                            submethod = '3c'
-                        else:
-                            submethod = '3b'
                     h['METHOD2'] = (submethod, 'exact optimal extraction method used')
                 #write to FITS file    
+                outfn = path + starname + '_' + obsname + '_' + method.lower() + submethod + '_extracted.fits'
                 pyfits.writeto(outfn, fluxarr, h, clobber=True)    
                 #now append the corresponding error array
                 h_err = h.copy()
@@ -929,7 +936,7 @@ def extract_spectrum(stripes, err_stripes, ron_stripes, method='optimal', indivi
                 extracted['pix'] = pix
                 extracted['flux'] = flux
                 extracted['err'] = err
-                np.save(path + obsname + '_extracted.npy', extracted)
+                np.save(path + starname + '_' + obsname + '_' + method.lower() + submethod + '_extracted.npy', extracted)
         
     return pix,flux,err
 
@@ -1016,6 +1023,21 @@ def extract_spectrum_from_indices(img, err_img, stripe_indices, method='optimal'
         
     #now save to FITS file or PYTHON DICTIONARY if desired
     if savefile:
+        
+        # which extraction method was used exactly?
+        submethod = ''
+        if method.lower() == 'optimal':
+            if individual_fibres:
+                submethod = '3a'
+            else:
+                if combined_profiles:
+                    submethod = '3c'
+                else:
+                    submethod = '3b'
+        
+        # name of object
+        starname = pyfits.getval(path+obsname+'.fits', 'OBJECT')
+                    
         if path is None:
             print('ERROR: path to output directory not provided!!!')
             return
@@ -1025,16 +1047,22 @@ def extract_spectrum_from_indices(img, err_img, stripe_indices, method='optimal'
         else:
             while filetype not in ["fits", "dict", "both"]:
                 print('ERROR: file type for output file not recognized!')
-                filetype = raw_input('Which method do you want to use (valid options are ["fits" / "dict" / "both"] )?') 
+                filetype = raw_input('Which file type do you want to use (valid options are ["fits" / "dict" / "both"] )?') 
             if filetype in ['fits', 'both']:
-                #OK, save as FITS file
-                outfn = path+obsname+'_extracted.fits'
-                fluxarr = np.zeros((len(pix), len(pix['order_01'])))
-                errarr = np.zeros((len(pix), len(pix['order_01'])))
-                for i,o in enumerate(sorted(pix.keys())):
-                    fluxarr[i,:] = flux[o]
-                    errarr[i,:] = err[o]
-                #try and get header from previously saved files
+                if method.lower() == 'optimal' and submethod == '3a':      # ie for individual-fibre extraction
+                    fluxarr = np.zeros((len(pix), len(flux['order_01']), len(pix['order_01'])))
+                    errarr = np.zeros((len(pix), len(flux['order_01']), len(pix['order_01'])))
+                    for i,o in enumerate(sorted(pix.keys())):
+                        for j,fib in enumerate(sorted(flux[o].keys())):
+                            fluxarr[i,j,:] = flux[o][fib]
+                            errarr[i,j,:] = err[o][fib]
+                else:
+                    fluxarr = np.zeros((len(pix), len(pix['order_01'])))
+                    errarr = np.zeros((len(pix), len(pix['order_01'])))
+                    for i,o in enumerate(sorted(pix.keys())):
+                        fluxarr[i,:] = flux[o]
+                        errarr[i,:] = err[o]
+                # try and get header from previously saved files
                 if os.path.exists(path+obsname+'_BD_CR_BG_FF.fits'):
                     h = pyfits.getheader(path+obsname+'_BD_CR_BG_FF.fits')
                 elif os.path.exists(path+obsname+'_BD_CR_BG.fits'):
@@ -1044,7 +1072,7 @@ def extract_spectrum_from_indices(img, err_img, stripe_indices, method='optimal'
                 elif os.path.exists(path+obsname+'_BD.fits'):
                     h = pyfits.getheader(path+obsname+'_BD.fits')
                 else:
-                    h = pyfits.getheader(path+obsname+'.fits')
+                    h = pyfits.getheader(path+obsname+'.fits')   
                 #update the header and write to file
                 h['HISTORY'] = '   EXTRACTED SPECTRUM - created '+time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())+' (GMT)'
                 h['METHOD'] = (method, 'extraction method used')
@@ -1055,15 +1083,9 @@ def extract_spectrum_from_indices(img, err_img, stripe_indices, method='optimal'
                 h['FIRSTORD'] = (topordnum, 'order number of first (top) order')
                 h['LASTORD'] = (botordnum, 'order number of last (bottom) order')
                 if method.lower() == 'optimal':
-                    if individual_fibres:
-                        submethod = '3a'
-                    else:
-                        if combined_profiles:
-                            submethod = '3c'
-                        else:
-                            submethod = '3b'
                     h['METHOD2'] = (submethod, 'exact optimal extraction method used')
                 #write to FITS file    
+                outfn = path + starname + '_' + obsname + '_' + method.lower() + submethod + '_extracted.fits'
                 pyfits.writeto(outfn, fluxarr, h, clobber=True)    
                 #now append the corresponding error array
                 h_err = h.copy()
@@ -1077,7 +1099,7 @@ def extract_spectrum_from_indices(img, err_img, stripe_indices, method='optimal'
                 extracted['pix'] = pix
                 extracted['flux'] = flux
                 extracted['err'] = err
-                np.save(path + obsname + '_extracted.npy', extracted)
+                np.save(path + starname + '_' + obsname + '_' + method.lower() + submethod + '_extracted.npy', extracted)
         
     return pix,flux,err
 

veloce_reduction/helper_functions.py
@@ -760,14 +760,21 @@ def short_filenames(file_list):
 
 
 
-def correct_orientation(img, orient=1):
+def correct_orientation(img, verbose=False):
     """
-    (1) = same orientation as the simulated spectra, ie wavelength decreases from left to right and bottom to top
+    bring image to same orientation as the simulated spectra, ie wavelength decreases from left to right and bottom to top
     """
-    if orient == 1:
+    
+    ny, nx = img.shape
+    
+    if (ny,nx) == (4112,4202):
         img = np.fliplr(img.T)
+    elif (ny,nx) == (4202,4112):
+        # this means the image is already in the correct orientation
+        if verbose:
+            print('The image is already in the correct orientation!')
     else:
-        print('ERROR: selected orientation not defined!!!')
+        print('ERROR: file shape not correct!!!')
     
     return img
 