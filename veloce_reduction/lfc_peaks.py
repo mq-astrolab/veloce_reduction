de0cf3a92ddc5119fca9e4d554890fb1df1462cf
cmbergmann <c.bergmann@unsw.edu.au>
2018-12-20 16:48:52 +1100

    vectorized the distance operation --> factor 30 faster

--- veloce_reduction/lfc_peaks.py
+++ veloce_reduction/lfc_peaks.py
@@ -11,12 +11,23 @@ import numpy as np
 from readcol import readcol
 
 path = '/Volumes/BERGRAID/data/veloce/lfc_peaks/'
+path = '/Users/christoph/data/lfc_peaks/'
 
 files = glob.glob(path + '*olc.nst')
 
 id, y0, x0, mag, err_mag, skymod, niter, chi, sharp, y_err, x_err = readcol(files[0], twod=False, skipline=2)
 id, y, x, mag, err_mag, skymod, niter, chi, sharp, y_err, x_err = readcol(files[1], twod=False, skipline=2)
 
+
+test_x0 = x0[(x0 > 1500) & (x0 < 1800) & (y0 > 1500) & (y0 < 1800)]
+test_y0 = y0[(x0 > 1500) & (x0 < 1800) & (y0 > 1500) & (y0 < 1800)]
+test_x = x[(x > 1500) & (x < 1800) & (y > 1500) & (y < 1800)]
+test_y = y[(x > 1500) & (x < 1800) & (y > 1500) & (y < 1800)]
+
+x0 = test_x0.copy()
+x = test_x.copy()
+y0 = test_y0.copy()
+y = test_y.copy()
 
 def find_affine_transformation_matrix(x, y, x0, y0, eps=0.5, timit=False):
     '''
@@ -33,40 +44,44 @@ def find_affine_transformation_matrix(x, y, x0, y0, eps=0.5, timit=False):
     
     # get numpy arrays from list of (x,y)-tuples in the shape (2,***)
     ref_peaks_xy_list = [(xpos, ypos) for xpos,ypos in zip(x0,y0)]
-    ref_peaks_xy = np.array(ref_peaks_xy_list).T
-    obs_peak_xy_list = [(xpos, ypos) for xpos,ypos in zip(x,y)]
-    obs_peaks_xy = np.array(obs_peak_xy_list).T
+    ref_peaks_xy = np.array(ref_peaks_xy_list).T     # not really needed
+    obs_peaks_xy_list = [(xpos, ypos) for xpos,ypos in zip(x,y)]
+    obs_peaks_xy = np.array(obs_peaks_xy_list).T
     
     #now we need to match the peaks
     good_ref_peaks = []
     good_obs_peaks = []
-    for refpeak in ref_peaks_xy_list[:10]:
-        # this works but is SUUUUPER slow!!!
+    for n,refpeak in enumerate(ref_peaks_xy_list):
+        # print(n)
         shifted_obs_peaks = obs_peaks_xy - np.expand_dims(np.array(refpeak), axis=1)
-        distance = np.zeros(shifted_obs_peaks.shape[1])
-        for i in range(len(distance)):
-            distance[i] = math.sqrt(shifted_obs_peaks[0,i]*shifted_obs_peaks[0,i] + shifted_obs_peaks[1,i]*shifted_obs_peaks[1,i])
-            
-        print(np.sum(distance < eps))
-        
-        
+        distance = np.sqrt(shifted_obs_peaks[0,:]**2 + shifted_obs_peaks[1,:]**2)
+
+        if np.sum(distance < eps) > 0:
+            if np.sum(distance < eps) > 1:
+                print('FUGANDA: ',refpeak)
+            good_ref_peaks.append(refpeak)
+            good_obs_peaks.append((obs_peaks_xy[0,distance < eps], obs_peaks_xy[1,distance < eps]))
+        print(n, refpeak, np.sum(distance < eps))
+
+
     #go to homogeneous coordinates (ie add a z-component equal to 1, so that we can include translation into the matrix)
-    ref_peaks_xyz = np.vstack((ref_peaks_xy, np.expand_dims(np.repeat(1, ref_peaks_xy.shape[1]), axis=0)))
-    obs_peaks_xyz = np.vstack((obs_peaks_xy, np.expand_dims(np.repeat(1, obs_peaks_xy.shape[1]), axis=0)))
+    good_ref_peaks_xyz = np.hstack((np.array(good_ref_peaks), np.expand_dims(np.repeat(1, len(good_ref_peaks)), axis=1)))
+    good_obs_peaks_xyz = np.hstack((np.squeeze(np.array(good_obs_peaks)), np.expand_dims(np.repeat(1, len(good_obs_peaks)), axis=1)))
     
-    #solve matrix equation: M*r1 = r2  (note the transpose, as linalg.lstsq wants row-vectors)
-    M, res, rank, s = np.linalg.lstsq(peaks1_xyz.T, peaks2_xyz.T)
-    
+    # np.linalg.lstsq(r1,r2) solves matrix equation M*r1 = r2  (note that linalg.lstsq wants row-vectors)
+    # i.e.: good_obs_peaks_xyz ~= np.dot(good_ref_peaks_xyz, Mtest)
+    M, res, rank, s = np.linalg.lstsq(good_ref_peaks_xyz, good_obs_peaks_xyz, rcond=None)
+
     if timit:
         print('Time elapsed: ' + str(np.round(time.time() - start_time,1)) + ' seconds')
     
-    #return affine transformation matrix  (note the transpose again, so that you can do :  new_points = np.dot(M.T,points) )
-    return M.T
+    #return affine transformation matrix  (note this is the transpose of the usual form eg as listed in wikipedia)
+    return M
 
 
 
-
-
+#   if   x' = x * M
+# then   x  = x' * M_inv
 
 
 
